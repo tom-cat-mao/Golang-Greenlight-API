d04c23c254025f49a9d10725cea6836a5ad9d715
Tom_Cat <maoy896@gmail.com>
2025-03-31 17:25:17 +0800

    Add helper function to read and validate "id" URL parameter
    Created a new helper function `readIDParam` in `helpers.go` to retrieve and validate the "id" parameter from the request context. Updated `showMovieHandler` in `movies.go` to utilize this new function for improved code clarity and error handling.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createMovieHandler for the "POST /v1/movies" endpoint
@@ -17,11 +14,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 // - Retrieve the interppolated "id" parameter from the current URL and include it in a placeholder
 // respose.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// Retrieve a slice containing the parameter names and values
-	params := httprouter.ParamsFromContext(r.Context())
-
 	// Get the value of the "id" parameters from the slice.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
